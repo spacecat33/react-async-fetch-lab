9b55f8e2d35bfbd745c6cf28ab070cf4af909899
Janan Akkad <jananakkad@hotmail.co.uk>
2023-05-07 11:41:18 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
 // create your App component here
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      peopleInSpace: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json')
+      .then(res => res.json())
+      .then(json => this.setState({peopleInSpace: json.people}))
+  }
+
+  makeLi(person){
+    return <li>Space-person {person.name} is currently on the {person.craft}</li>
+  }
+
+  render(){
+    return(
+      <div>
+        <div>Here are some Space Persons...</div>
+        <br />
+        <div>
+          {this.state.peopleInSpace.map(person => this.makeLi(person))}
+        </div>
+      </div>
+    )
+  }
+}
